0952c064c619999fdf74f04a0d0e3d2bfe6659c1
marcosdellavecchia <marcosdvecchia@gmail.com>
2020-09-20 20:48:58 -0300

    Se comentó el código

--- src/dnaTest.js
+++ src/dnaTest.js
@@ -9,16 +9,17 @@ function isMutant(dnaString) {
 
   // 1. Busca de forma horizontal la base nitrogenada C
 
-  // a. Recorre cada fila de la matriz (hasta la longitud de la cantidad de filas)
+  // a. Recorre cada fila de la matriz
   for (i = 0; i < dnaString.length; i++) {
-    // b. Recorre las bases nitrogenadas dentro de cada fila
+    // b. Recorre las bases nitrogenadas dentro de cada fila (columnas) en las posiciones 0, 1 y 2
     for (j = 0; j <= dnaString[i].length - matchC.length; j++) {
       dnaToAnalyse = [];
       // c. Agrega a una variable arreglos de 4 bases nitrogenadas para analizar (de forma horizontal)
+      // Va agregando las posiciones 0, 1, 2, 3. Luego se corre y agrega las posiciones 1, 2, 3, 4. Asi va recorriendo cada FILA en grupos de 4.
       for (k = 0; k < matchC.length; k++) {
         dnaToAnalyse.push(dnaString[i][j + k]);
       }
-      // d. Une los arreglos de letras para compararlos con las combinaciones buscadas en mutantes. Si hay coincidencia, suma 1 al contador de bases nitrogenadas C
+      // d. Une los arreglos de 4 letras agregados para compararlos con las combinaciones buscadas en mutantes. Si hay coincidencia, suma 1 al contador de bases nitrogenadas C
       if (dnaToAnalyse.join() === matchC.join()) {
         counterC++;
       }
@@ -27,41 +28,30 @@ function isMutant(dnaString) {
 
   // 2. Busca de forma vertical la base nitrogenada G
 
-  // a. Recorre cada fila de la matriz (hasta la longitud de letras en cada fila)
+  // a. Recorre las bases dentro de cada fila de la matriz (columnas)
   for (i = 0; i < dnaString[0].length; i++) {
-    // b. Recorre las bases nitrogenadas dentro de cada fila
+    // b. Recorre las filas de la matriz en las posiciones 0, 1 y 2
     for (j = 0; j <= dnaString.length - matchG.length; j++) {
       dnaToAnalyse = [];
       // c. Agrega a una variable arreglos de 4 bases nitrogenadas para analizar (ordenado de forma vertical)
+      // Va agregando las posiciones 0, 1, 2, 3. Luego se corre y agrega las posiciones 1, 2, 3, 4. Asi va recorriendo cada COLUMNA en grupos de 4.
+
       for (k = 0; k < matchG.length; k++) {
         dnaToAnalyse.push(dnaString[j + k][i]);
       }
-      // d. Une los arreglos de letras para compararlos con las combinaciones buscadas en mutantes. Si hay coincidencia, suma 1 al contador de bases nitrogenadas G
+      // d. Une cada arreglo de 4 letras para compararlos con las combinaciones buscadas en mutantes. Si hay coincidencia, suma 1 al contador de bases nitrogenadas G
       if (dnaToAnalyse.join() === matchG.join()) {
         counterG++;
       }
     }
   }
 
-  // 3. Busca de forma oblicua la base nitrogenada A
+  // 3. Busca de forma oblicua la base nitrogenada A (de derecha a izquierda, de arriba hacia abajo)
   for (i = 0; i <= dnaString.length - matchA.length; i++) {
     for (j = 0; j <= dnaString[i].length - matchA.length; j++) {
       dnaToAnalyse = [];
       for (k = 0; k < matchA.length; k++) {
         dnaToAnalyse.push(dnaString[i + k][j + k]);
-      }
-      if (dnaToAnalyse.join() === matchA.join()) {
-        counterA++;
-      }
-    }
-  }
-
-  // 4. Busca de forma oblicua la base nitrogenada A (desde el otro extremo de la matriz)
-  for (i = 0; i <= dnaString.length - matchA.length; i++) {
-    for (j = dnaString[i].length - 1; j >= 0 + matchA.length - 1; j--) {
-      dnaToAnalyse = [];
-      for (k = 0; k < matchA.length; k++) {
-        dnaToAnalyse.push(dnaString[i + k][j - k]);
       }
       if (dnaToAnalyse.join() === matchA.join()) {
         counterA++;
